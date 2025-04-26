e3b47df8eb5618ff6177371d63e33d25eb5a0ee6
Rudransh Bhardwaj <249.rudranshbhardwaj@gmail.com>
2025-04-26 11:35:26 +0530

    updated colour and added validation for url

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,34 @@ import React, { useState } from "react";
 function App() {
   const [value, setValue] = useState("");
   const [shortUrl, setShortUrl] = useState("");
+  const [error, setError] = useState("");
 
   const handleShorten = async () => {
-    const res = await fetch(
-      "https://url-shortner-backend-ssdt.onrender.com/api/url/save",
-      {
+    if (!value.trim()) {
+      setError("Please enter a valid URL.");
+      setShortUrl("");
+      return;
+    }
+
+    setError("");
+
+    try {
+      const res = await fetch("http://localhost:8080/api/url/save", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ value }),
-      }
-    );
+      });
 
-    const data = await res.text(); // because backend returns plain string
-    setShortUrl(data);
+      const data = await res.text();
+      setShortUrl(data);
+    } catch (error) {
+      console.error("Error shortening URL:", error);
+      setError("Something went wrong. Please try again.");
+    }
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+    <div className="min-h-screen flex items-center justify-center bg-gray-600 p-4">
       <div className="bg-white p-6 rounded shadow-md w-full max-w-md">
         <h1 className="text-2xl font-bold mb-4">Short URL Generator</h1>
 
@@ -30,12 +41,16 @@ function App() {
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
+
+        {error && <p className="text-red-600 font-semibold mb-2">{error}</p>}
+
         <button
-          className="bg-blue-600 text-white px-4 py-2 rounded w-full hover:bg-blue-700 transition mb-4"
+          className="bg-blue-600 text-white px-4 py-2 rounded w-full active:bg-blue-900 hover:bg-blue-700 transition mb-4 hover:cursor-pointer"
           onClick={handleShorten}
         >
           Shorten URL
         </button>
+
         {shortUrl && (
           <p className="text-green-600 font-semibold mb-4 break-all">
             Shortened URL: {shortUrl}
